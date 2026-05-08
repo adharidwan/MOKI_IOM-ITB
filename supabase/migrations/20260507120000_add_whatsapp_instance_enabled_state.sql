1adc5522708c2589e5281dce9f4fe5928aa5f070
Muhammad Adha Ridwan <140234882+adharidwan@users.noreply.github.com>
2026-05-08 20:40:47 +0700

    delete: deleteunused migrations

--- supabase/migrations/20260507120000_add_whatsapp_instance_enabled_state.sql
+++
@@ -1,6 +0,0 @@
-alter table public.whatsapp_instances
-  add column if not exists is_enabled boolean not null default true;
-
-update public.whatsapp_instances
-set is_enabled = true
-where is_enabled is null;
--- supabase/migrations/20260508120000_add_whatsapp_instance_retired_at.sql
+++
@@ -1,2 +0,0 @@
-alter table public.whatsapp_instances
-  add column if not exists retired_at timestamptz;
